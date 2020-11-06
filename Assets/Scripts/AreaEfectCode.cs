92f966667636d3a485c9cd85d52eccd87ec3b9ea
Andres Arias <andres10333@hotmail.com>
2020-11-06 18:22:06 -0500

    initial commit

---
+++ Assets/Scripts/AreaEfectCode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEfectCode : MonoBehaviour {
+    public AreaEffector2D example;
+    private Event lol;
+
+    private void Start () {
+        lol = GameObject.Find ("Cube").GetComponent<Event> ();
+        example = GetComponent<AreaEffector2D> ();
+    }
+
+    private void Update () {
+        if (lol.lol1) {
+            example.angularDrag = 0f;
+            example.drag = 0f;
+            example.forceAngle = 0f;
+            example.forceMagnitude = 10f;
+            example.forceVariation = 0f;
+            example.useGlobalAngle = true;
+        } else {
+            example.angularDrag = 0f;
+            example.drag = 0f;
+            example.forceAngle = 0f;
+            example.forceMagnitude = -10f;
+            example.forceVariation = 0f;
+            example.useGlobalAngle = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event : MonoBehaviour {
+    public bool lol1 = false;
+    private void OnTriggerEnter2D (Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            lol1 = true;
+        }
+
+    }
+    private void OnTriggerExit2D (Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            lol1 = false;
+        }
+    }
+}
